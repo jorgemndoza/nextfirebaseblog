2fbecf8e1b43f4fd6eec79da0993ef30c7dac10e
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2021-04-15 17:41:15 +0700

    set up react-hot-toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import { Toaster } from 'react-hot-toast'
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar /> <Component {...pageProps} />
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
+import { formatWithValidation } from "next/dist/next-server/lib/utils";
 
 export default MyApp
 
-// import Navbar from "../components/Navbar";
+
