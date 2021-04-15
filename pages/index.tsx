dd9ccc136716f07443bc80d512a501aa40f47d03
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2021-04-15 17:05:47 +0700

    added visual loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{pathname: '/[username]', 
-                  query: { username: 'david30'}
-                  }}>
-              <a>Davids Profile</a>
-          </Link>
+      <Loader show /> 
     </div>
   )
 }
