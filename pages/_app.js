f61acc323298c54780714eab31c84fc26e806f60
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2021-04-17 01:52:14 +0700

    pushed with error for troubleshooting purposes. getting "ReferenceError: auth is not defined"

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,32 @@ import Navbar from "../components/Navbar";
 import { Toaster } from 'react-hot-toast'
 import { UserContext } from '../lib/context'
 
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth'
+
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth)
+  const [username, setUsername] = useState(null)
+
+  useEffect(() => {
+
+let unsubscribe; 
+
+if (user) {
+  const ref = firestore.collection('users').doc(user.uid);
+  unsubscribe = ref.onSnapshot((doc) => {
+    setUsername(doc.data()?.username)
+  })
+} else {
+  setUsername(null)
+}
+return unsubscribe
+  }, [user])
+
+
   return (
-      <UserContext.Provider value={{ user:{}, username: 'david'}}>
+      <UserContext.Provider value={{ user, username}}>
         <Navbar />
         <Component {...pageProps} />
         <Toaster />
